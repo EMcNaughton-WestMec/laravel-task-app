c3706a47e2444bebc97a6ceb379116cf7aa0c90d
Alopez647 <142362591+Alopez647@users.noreply.github.com>
2025-02-20 23:42:34 +0000

    day 3

--- taskApp/database/seeders/TaskSeeder.php
+++ taskApp/database/seeders/TaskSeeder.php
@@ -43,13 +43,23 @@ Task::create([
 ]);
 Task::create([
 
-'task_name' => '',
-'task_location' => 'Kitchen',
-'time_complexity' => 1, // (1 => ~10 min, 2 => ~30 min, etc.)
-'materials_required'=> 'Trash bags',
-'deadline' => '2025-02-20 10:00:00',
+'task_name' => 'Walk the Dog',
+'task_location' => 'Neighborhood',
+'time_complexity' => 2, // (1 => ~10 min, 2 => ~30 min, etc.)
+'materials_required'=> 'Leash, dog',
+'deadline' => '2025-02-20 18:00:00',
 'priority' => 2, // (1 => low, 2 => medium, 3 => high)
 'category' => 'chores'
+]);
+Task::create([
+
+'task_name' => 'Finish Documents',
+'task_location' => 'Office',
+'time_complexity' => 5, // (1 => ~10 min, 2 => ~30 min, etc.)
+'materials_required'=> 'Laptop',
+'deadline' => '2025-02-22 20:00:00',
+'priority' => 3, // (1 => low, 2 => medium, 3 => high)
+'category' => 'Work'
 ]);
 }
 }
